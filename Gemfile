c8241f43c8d0b307b20cc786e98e19284a7a249e
Thiago Arrais <thiago.arrais@gmail.com>
2017-04-07 17:52:02 +0000

    hello, recaptcha!

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem "sinatra", "~> 1.4"
 gem "thin", "~> 1.7"
+gem "faraday"
+gem "json"
--- app.rb
+++ app.rb
@@ -1,7 +1,37 @@
 require 'sinatra'
+require 'faraday'
+require 'json'
 
 class Webapp < Sinatra::Base
   get '/' do
-    "hello, world!"
+    "vá para /index.html"
+  end
+  
+  post '/' do
+    if recaptcha(params['g-recaptcha-response'], request.ip) then
+      <<-HTML
+        <html>
+          <body>
+            #{params['nome']}, eu acho que você não é um robô.
+            registrei o email #{params['email']} na minha base de dados!
+          </body>
+        </html>"
+      HTML
+    else
+      "not ok"
+    end
+  end
+  
+  def recaptcha(response_token, ip)
+    resp = Faraday.post(
+      'https://www.google.com/recaptcha/api/siteverify',
+      secret: '6LduABwUAAAAAJ7hscZJjNhlBvkGeFUBTxUezFMa',
+      response: response_token,
+      remoteip: ip
+    )
+    result = JSON.parse(resp.body)
+    puts "result for #{response_token}"
+    p result
+    !!result['success']
   end
 end
